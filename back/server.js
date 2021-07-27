2330ed533c277bcda021fa680746760edf3d67d4
ArunavaChowdhury <arunav.pavel@gmail.com>
2021-07-27 18:10:29 -0400

    update and cleanup

--- back/server.js
+++ back/server.js
@@ -1,3 +1,4 @@
+//Added dippendancies
 const express = require("express");
 const bodyParser = require("body-parser");
 const util = require("util");
@@ -7,12 +8,17 @@ const http = require("http");
 const cors = require("cors");
 require("dotenv").config();
 
+//Setting up express server
+// Decleared surver running port number
+// Decleared bearer tocken
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 const BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
 const get = util.promisify(request.get);
 
+//CORS is adde as dependancy and allowed response from all origin
+// Allowed access contraol from all origin in the header
 var corsOptions = {
   origin: "*",
 };
@@ -31,6 +37,7 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Function to send authorization with the tocken to get response from twitter api
 let getTweet = async (req, res, streamURL) => {
   const requestConfig = {
     url: streamURL,
@@ -52,6 +59,7 @@ let getTweet = async (req, res, streamURL) => {
   }
 };
 
+//Endpoint to get latest 20 tweet from joe Biden's timeline
 app.get("/api/tweet/Joe-Biden", async (req, res) => {
   const streamURL = new URL(
     "https://api.twitter.com/2/users/1349149096909668363/tweets?pagination_token=7140dibdnow9c7btw3z16xcn54ryhshh2zyfxg4lctfeh&tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=20"
@@ -59,6 +67,7 @@ app.get("/api/tweet/Joe-Biden", async (req, res) => {
   return getTweet(req, res, streamURL);
 });
 
+//Endpoint to get latest 20 tweet from Hilari Clinton's timeline
 app.get("/api/tweet/Hilari-Clinton", async (req, res) => {
   const streamURL = new URL(
     "https://api.twitter.com/2/users/1339835893/tweets?pagination_token=7140dibdnow9c7btw3z16xcn54ryhshh2zyfxg4lctfeh&tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=20"
@@ -66,5 +75,8 @@ app.get("/api/tweet/Hilari-Clinton", async (req, res) => {
   return getTweet(req, res, streamURL);
 });
 
+//Error message
 app.use((req, res) => res.status(404).type("txt").send("🤷‍♂️"));
+
+//Server running port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
