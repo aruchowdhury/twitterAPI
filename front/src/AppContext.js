4f43b617619e22d2dd13745b544cec8f9e728c8b
ArunavaChowdhury <arunav.pavel@gmail.com>
2021-07-27 03:21:46 -0400

    added flipcard

--- front/src/AppContext.js
+++ front/src/AppContext.js
@@ -6,16 +6,34 @@ export const AppContext = createContext(null);
 export const AppProvider = ({ children }) => {
   //Created toggle state to switch between components from header
   const [toggle, setToggle] = useState(false);
+
+  //Created flip state to flip header
+  const [isFlipped, setIsFlipped] = useState(false);
+
   // Created tweet to hold the received data from fetch
   const [tweet, setTweet] = useState([]);
+
   // function to switch screen/components from header
   const handleToggle = () => {
     setToggle(!toggle);
   };
+  // function to flip header
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   return (
     <AppContext.Provider
-      value={{ toggle, setToggle, handleToggle, tweet, setTweet }}
+      value={{
+        toggle,
+        setToggle,
+        handleToggle,
+        tweet,
+        setTweet,
+        isFlipped,
+        setIsFlipped,
+        handleFlip,
+      }}
     >
       {children}
     </AppContext.Provider>
--- front/src/Header.js
+++ front/src/Header.js
@@ -2,62 +2,57 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { AppContext } from "./AppContext";
 import { Link } from "react-router-dom";
-
+import ReactCardFlip from "react-card-flip";
 const Header = () => {
   //Receiving states using react context
-  const { toggle, handleToggle } = useContext(AppContext);
+  const { isFlipped, handleFlip } = useContext(AppContext);
 
-  //Using toggle state and handleToggle function to switch screens/components
+  //Used react-card-flip to flip header
+  //Using isFlipped state and handleFlip function to switch screens/components
   return (
-    <Wrapper>
-      {toggle ? (
-        <Link onClick={handleToggle} to="/">
-          <NameDiv>Joe Biden's Tweets</NameDiv>
-        </Link>
-      ) : (
-        <Link onClick={handleToggle} to="/biden">
-          <NameDiv>Hilari Clinton's Tweets</NameDiv>
-        </Link>
-      )}
-    </Wrapper>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <Link to="/">
+        <NameDiv onClick={handleFlip}>Joe Biden's Tweets</NameDiv>
+      </Link>
+
+      <Link to="/biden">
+        <NameDiv onClick={handleFlip}>Hilari Clinton's Tweets</NameDiv>
+      </Link>
+    </ReactCardFlip>
   );
 };
 
 export default Header;
 
-const Wrapper = styled.div`
-  height: 5rem;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  font-size: 1.2rem;
-  margin-left: 5px;
-  margin-top: 5px;
-  /* background-color: #b8b5ff; */
-  @media screen and (max-width: 800px) {
-    display: flex;
-    justify-content: center;
-  }
-`;
+// const Wrapper = styled.div`
+//   height: 5rem;
+//   width: 100%;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   flex-wrap: wrap;
+//   font-size: 1.2rem;
+//   margin-left: 5px;
+//   margin-top: 5px;
+//   /* background-color: #b8b5ff; */
+//   @media screen and (max-width: 800px) {
+//     display: flex;
+//     justify-content: center;
+//   }
+// `;
 
 const NameDiv = styled.button`
   height: 100%;
   width: 100%;
-  padding: 1.6rem 1.2rem;
+  padding: 1.8rem 1.2rem;
   border: none;
-  border-radius: 0.5rem;
   background-color: #867ae9;
-  font-size: 2rem;
+  font-size: 2.5rem;
   color: #21094e;
   &:hover {
-    transition: transform 0.7s ease-in-out;
-    transform: translate(3rem);
-    background-color: #786dd0;
+    transition: 0.7s ease-in-out;
+    opacity: 0.85;
   }
-  transition: transform 0.7s ease-in-out;
-  transform: rotate(150);
   @media screen and (max-width: 800px) {
     &:hover {
       transition: none;
